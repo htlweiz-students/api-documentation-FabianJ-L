6d77da8ce54ae28b34b6bfc94cc4211a8f9fbcee
Student <foobar@sr.htlweiz.at>
2025-12-15 10:54:00 +0100

    .

---
+++ AverageCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace AverageCalculator.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ AverageCalculator/Class1.cs
@@ -0,0 +1,42 @@
+﻿namespace AverageCalculator;
+
+public class Class1
+{
+    private readonly List<double> list = new List<double>(100);
+
+    public Class1(double[] values)
+    {
+        list.AddRange(values);
+    }
+    public void Add(double value)
+    {
+        list.Add(value);
+    }
+    public int Add(double[] values)
+    {
+        if (values.Length == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            list.AddRange(values);
+            return 0;
+        }
+    }
+    public double getAverage()
+    {
+        if (list.Count == 0)
+            throw new InvalidOperationException("Keine Werte vorhanden");
+
+        return list.Average();
+    }
+    public double[] getElemnts()
+    {
+        return list.ToArray();
+    }
+    public int count(){
+        return list.Count(); 
+    }
+}
+
